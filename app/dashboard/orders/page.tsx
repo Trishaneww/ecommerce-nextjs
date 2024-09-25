7d2b40ae066c7f3a445ba428de21c60b2423b874
Trishane <trishaneww@gmail.com>
2024-09-25 00:45:59 -0400

    orders table

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,4 +1,5 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function OrdersPage() {
     return (
@@ -7,6 +8,31 @@ export default function OrdersPage() {
                 <CardTitle>Orders</CardTitle>
                 <CardDescription>Recent orders from your store</CardDescription>
             </CardHeader>
+            <CardContent>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Customer</TableHead>
+                            <TableHead>Type</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Date</TableHead>
+                            <TableHead className="text-right">Amount</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>
+                                <p className="font-medium">Trishane Wijeyawickrema</p>
+                                <p className="hidden md:flex text-sm text-muted-foreground">test@test.com</p>
+                            </TableCell>
+                            <TableCell>Sale</TableCell>
+                            <TableCell>Succesful</TableCell>
+                            <TableCell>2024/06/16</TableCell>
+                            <TableCell className="text-right">$250.00</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </CardContent>
         </Card>
     )
 }
