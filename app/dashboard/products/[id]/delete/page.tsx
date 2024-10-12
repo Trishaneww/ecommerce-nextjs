c4436bf476c2daf270b2ecbb144055e0959347b2
Trishane <trishaneww@gmail.com>
2024-10-12 02:45:58 -0400

    created delete route

--- app/dashboard/products/[id]/delete/page.tsx
+++ app/dashboard/products/[id]/delete/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function DeleteRoute() {
     return (
-        <div>
-            
+        <div className="h-[80vh] w-full flex items-center justify-center">
+            <Card className="max-w-xl">
+                <CardHeader>
+                    <CardTitle>Are your absolutely sure?</CardTitle>
+                    <CardDescription>
+                        This action cannot be undone. This will permanently delete this product and remove all data from our servers
+                    </CardDescription>
+                </CardHeader>
+                <CardFooter className="w-full flex justify-between">
+                    <Button variant="secondary">Cancel</Button>
+                    <Button variant="destructive">Continue</Button>
+                </CardFooter>
+            </Card>
+
         </div>
     )
 }
